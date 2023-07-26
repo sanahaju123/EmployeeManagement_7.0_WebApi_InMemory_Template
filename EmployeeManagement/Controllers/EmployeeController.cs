6d0ca93f538d431aacf099862916deb33494ce97
user <sanahaju123@gmail.com>
2023-07-26 16:53:52 +0530

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.BusinessLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        [Route("/employees")]
+        public IActionResult GetEmployees()
+        {
+            //Write Your Code Here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,35 @@
+using EmployeeManagement.BusinessLayer.Interfaces;
+using EmployeeManagement.BusinessLayer.Services.Repository;
+using EmployeeManagement.BusinessLayer.Services;
+using EmployeeManagement.DataLayer;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+           options.UseInMemoryDatabase("InMemoryDatabase"));
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
